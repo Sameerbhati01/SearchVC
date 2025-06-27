decc823d0b9026984422446502dfbabf66a3e72b
Sameer Bhati <bhati.sameer987@gmail.com>
2025-06-27 11:05:35 +0530

    Initial Commit

---
+++ SearchVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchVC
+//
+//  Created by Sameer Bhati on 27/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SearchVCTests/SearchVCTests.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVCTests.swift
+//  SearchVCTests
+//
+//  Created by Sameer Bhati on 27/06/25.
+//
+
+import Testing
+@testable import SearchVC
+
+struct SearchVCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
